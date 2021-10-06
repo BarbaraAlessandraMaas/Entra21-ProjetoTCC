5fabf12c6320f95c0db45be9c652706b56e58127
Barbara <barbaraalessandramaas@gmail.com>
2021-10-06 11:59:04 -0300

    Finalização Banco de Dados

---
+++ server/src/db/migrations/20211006141601-create-pagamento.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('pagamentos', {
+      cd_pagamento: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        defaultValue: DataTypes.UUIDV4
+      },
+      ds_status: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      dt_vencimento: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('pagamentos');
+  }
+};
