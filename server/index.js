c5a2bc81f904e27ad939069930e59a65be716f91
Vitor <vitorhoesouza@gmail.com>
2021-10-08 11:53:28 -0300

    Início criação das rotas

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Iportando as rotas
+const usersRoutes = require("./routes/usersRoutes");
+
+// Definindo os middlewares
+app.use(express.json());
+
+// Definindo as rotas
+app.use("/users", usersRoutes);
+
+// Testando se o servidor está rodando
+app.listen(PORT, () => console.log("O servidor esta rodando"));
---
+++ server/models/users.js
@@ -0,0 +1,6 @@
+// Banco de dados
+module.exports = [
+    { id: 1, name: "Joabson", email: "joabson@email.com" },
+    { id: 2, name: "Cleber", email: "cleber@email.com" },
+    { id: 1, name: "Paulo", email: "paulo@email.com" },
+];
