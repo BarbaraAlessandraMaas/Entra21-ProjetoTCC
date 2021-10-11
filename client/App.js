cb993ae1149f0b59428e2946fe56ccd9c65e2439
Barbara <barbaraalessandramaas@gmail.com>
2021-10-10 23:55:41 -0300

    Criação Tela de Planos

--- client/App.js
+++ client/App.js
@@ -1,13 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TelaPlanos } from './screens/TelaPlanos';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <TelaPlanos/>
   );
 }
 
---
+++ client/screens/TelaPlanos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export function TelaPlanos() {
+    return(
+        <View>
+            <Text>
+                oi
+            </Text>
+        </View>
+    );
+}
