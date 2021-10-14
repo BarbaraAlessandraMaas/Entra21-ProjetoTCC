c2103541beb9b8011734a43d856a1fd6344f38dc
Barbara <barbaraalessandramaas@gmail.com>
2021-10-14 18:30:25 -0300

    Finalização Tela de Opções

--- client/App.js
+++ client/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { TelaAjuda } from './screens/telaAjuda';
+import { TelaAjuda } from './screens/TelaAjuda';
 import { TelaOpcoes } from './screens/TelaOpcoes';
+import { TelaTopico } from './screens/TelaTopico';
 
 export default function App() {
   return (
     // <TelaOpcoes />
-    <TelaAjuda />
+    // <TelaAjuda />
+    <TelaTopico/>
   );
 }
 
