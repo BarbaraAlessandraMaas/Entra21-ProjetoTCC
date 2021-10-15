142d1387d344062f9342a0775107f3fc787aee16
Barbara <barbaraalessandramaas@gmail.com>
2021-10-15 00:36:37 -0300

    Criação e Desenvolvimento Tela de Endereço

--- client/App.js
+++ client/App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { TelaCidade } from './screens/TelaCidade';
+import { TelaEndereco } from './screens/TelaEndereco';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TelaEndereco/>
+    // <TelaCidade/>
   );
 }
 
